cd700e1eacfec95c2149688eea34e580294f1ca9
Sudharshan <sudhar.ti@gmail.com>
2014-11-22 15:25:22 -0600

    Move dropzone into bootstrap modal

--- app/assets/javascripts/media_contents.js
+++ app/assets/javascripts/media_contents.js
@@ -5,6 +5,6 @@ $(function() {
     var imageUrl;
     imageUrl = responseText.file_name.url;
     $("#media-contents").append('<div class="col-lg-4"><div class="thumbnail"><img src="' + imageUrl + '"/></div></div>');
-    console.log(imageUrl);
+    $('#myModal').modal('hide');
   });
 });
