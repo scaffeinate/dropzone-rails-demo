b9063880e4d5e0e883d2aebf5c737aa73e15bdc5
Sudharshan <sudhar.ti@gmail.com>
2014-11-22 14:29:59 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'media_contents#index'
+
+  resources :media_contents, only: [:create]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
