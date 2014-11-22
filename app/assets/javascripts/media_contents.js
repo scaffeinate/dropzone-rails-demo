36bb7be0318a0bf23fc24c220999638ca6bf5f1f
Sudharshan <sudhar.ti@gmail.com>
2014-11-22 16:58:27 -0600

    Handle after dropzone upload completes

--- app/assets/javascripts/media_contents.js
+++ app/assets/javascripts/media_contents.js
@@ -2,9 +2,22 @@ $(function() {
   var mediaDropzone;
   mediaDropzone = new Dropzone("#media-dropzone");
   return mediaDropzone.on("success", function(file, responseText) {
-    var imageUrl;
-    imageUrl = responseText.file_name.url;
-    $("#media-contents").append('<div class="col-lg-4"><div class="thumbnail"><img src="' + imageUrl + '"/></div></div>');
-    $('#myModal').modal('hide');
+    var _this = this;
+    appendContent(responseText.file_name.url, responseText.id);
+    setTimeout(function(){
+      $('#myModal').modal('hide');
+      _this.removeAllFiles();
+    },1000);
   });
 });
+
+var appendContent = function(imageUrl, mediaId) {
+  $("#media-contents").append('<div class="col-lg-4">' + 
+    '<div class="thumbnail"><img src="' + imageUrl + '"/>' +
+    '<div class="caption">' +
+    '<input id="media_contents_" name="media_contents[]" value="' + mediaId +'" type="checkbox">' + 
+    '</div>' +
+    '</div></div>');
+  $("#delete").removeAttr('disabled');
+  $("#no-media").html("");
+};
