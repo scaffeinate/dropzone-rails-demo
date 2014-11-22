55cd9af0d8bcf6bec54910588c415cb6f91b8d32
Sudharshan <sudhar.ti@gmail.com>
2014-11-22 17:23:47 -0600

    Add cloudinary support for heroku

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'spring',        group: :development
 gem 'dropzonejs-rails'
 
 gem 'carrierwave'
+gem 'cloudinary'
 
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -6,6 +6,8 @@ class MediaUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  include Cloudinary::CarrierWave
+  
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
